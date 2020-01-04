047c11919639f247f6ff4b0d42028a59078808d8
Rania Ben Said <raniabensaid.rbs@gmail.com>
2020-01-04 15:09:29 +0100

    nouveau controlleur

--- projet/src/Controllers/HomeController.php
+++ projet/src/Controllers/HomeController.php
@@ -4,4 +4,17 @@
 class HomeController
 {
 
+    public function getInscriptionForm($app){
+        $ins = new Inscrit();
+        //si le pseudo est valide
+        $ins->pseudo = $_POST["username"] ;
+        $ins->nom = $_POST["name"] ;
+        $ins->adresse = $_POST["address"] ;
+        $ins->email = $_POST["mail"] ;
+        $ins->mobile = $_POST["mobile"] ;
+
+        $ins->save();
+        $app->redirect("/menu");
+    }
+
 }
